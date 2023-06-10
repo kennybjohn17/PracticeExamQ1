0815c1ec476040f3c502c478f6d46665153bef5d
kennybjohn17 <kennybrianjohn17@gmail.com>
2023-06-10 03:44:39 +0000

    wish me luck

--- public/databases/database.sql
+++ public/databases/database.sql
@@ -1,5 +1,14 @@
+SHOW Databases;
+CREATE DATABASE shoes;
+USE shoes;
 CREATE TABLE items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT
-);
+    ->     id INT AUTO_INCREMENT PRIMARY KEY,
+    ->     name VARCHAR(255) NOT NULL,
+    ->     description TEXT
+    -> );
+
+INSERT INTO items (id, name, description) VALUES (1,"Shoe 1","A shoe");
+INSERT INTO items (id, name, description) VALUES (2,"Shoe 2","Another shoe");
+INSERT INTO items (id, name, description) VALUES (3,"Shoe 3","Another shoe");
+INSERT INTO items (id, name, description) VALUES (4,"Shoe 4","Another shoe");
+
