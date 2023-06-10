94a7a000b5d9105f4a56183958b793b502c45fe5
kennybjohn17 <kennybrianjohn17@gmail.com>
2023-06-10 05:30:34 +0000

    asdfghjkl

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,13 +7,28 @@ $(document).ready(() => {
         // Process the received JSON data
         data.forEach((element) => {
           // Create a new HTML element for each data item
-          const newElement = $('<div>').addClass('data-element').text(element.name);
-          const newElement1 = $('<div>').addClass('data-description').text(element.description);
+          const newElement1 = $('<h1>').addClass('data-element').text(element.name);
+          const newElement2 = $('<p>').addClass('data-description').text(element.description);
 
           // Append the element to the output container
-          $('#output').append(newElement);
-          $('#output').append(newElement1);
+          $('.output').append(newElement1,newElement2);
         });
       }
     });
   });
+
+function search() {
+  var searchQuery = document.getElementById('find').value.toLowerCase();
+  var elements = document.getElementsByClassName('data-element');
+
+    for (var i = 0; i < elements.length; i++) {
+    var elementText = elements[i].textContent.toLowerCase();
+
+    if (elementText.includes(searchQuery)) {
+      elements[i].classList.remove('hidden');
+    } else {
+      elements[i].classList.add('hidden');
+    }
+  }
+}
+
