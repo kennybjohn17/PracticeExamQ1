3d24b91e431a1579175dbe5ee2e8ca0ce25a2549
kennybjohn17 <kennybrianjohn17@gmail.com>
2023-06-10 06:57:05 +0000

    searching is nearly finished

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,13 +8,13 @@ $(document).ready(() => {
         data.forEach((element) => {
           // Create a new HTML element for each data item
           const newElement1 = $('<h1>').addClass('data-name').text(element.name);
+          const newElement4 = $('<div>').addClass('data-brand').text(element.brand);
           const newElement2 = $('<p>').addClass('data-description').text(element.description);
-          const newElement3 = $('<p>').addClass('data-size').text(element.size);
-          const newElement4 = $('<p>').addClass('data-brand').text(element.brand);
-          const newElement5 = $('<p>').addClass('data-price').text(element.price);
-          const newElement6 = $('<p>').addClass('data-label1').text(element.label1);
-          const newElement7 = $('<p>').addClass('data-label2').text(element.label2);
-          const newElement8 = $('<p>').addClass('data-label3').text(element.label3);
+          const newElement3 = $('<label>').addClass('data-size').text(element.size);
+          const newElement6 = $('<label>').addClass('data-label1').text(element.label1);
+          const newElement7 = $('<label>').addClass('data-label2').text(element.label2);
+          const newElement8 = $('<label>').addClass('data-label3').text(element.label3);
+          const newElement5 = $('<div>').addClass('data-price').text(element.price);
 
           // Append the element to the output container
           $('.output').append(newElement1,newElement2,newElement3,newElement4,newElement5,newElement6,newElement7,newElement8);
@@ -35,7 +35,7 @@ function search() {
   var label3 = document.getElementsByClassName('data-label3');
 
     for (var i = 0; i < names.length; i++) {
-    var Text = names[i].textContent.toLowerCase() + sizes[i].textContent.toLowerCase() + brands[i].textContent.toLowerCase();
+    var Text = names[i].textContent.toLowerCase() + sizes[i].textContent.toLowerCase() + brands[i].textContent.toLowerCase() + label1[i].textContent.toLowerCase() + label2[i].textContent.toLowerCase() + label3[i].textContent.toLowerCase();
 
     if (Text.includes(search)) {
       names[i].classList.remove('hidden');
