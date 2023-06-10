9b1d5a92f9cb811a514eae06240bca673ffb5dbf
kennybjohn17 <kennybrianjohn17@gmail.com>
2023-06-10 04:26:31 +0000

    change query

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,9 +8,11 @@ $(document).ready(() => {
         data.forEach((element) => {
           // Create a new HTML element for each data item
           const newElement = $('<div>').addClass('data-element').text(element.name);
+          const newElement1 = $('<div>').addClass('data-description').text(element.description);
 
           // Append the element to the output container
           $('#output').append(newElement);
+          $('#output').append(newElement1);
         });
       }
     });
